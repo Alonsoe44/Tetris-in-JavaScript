775a236a279093e37ed4e60f3ec0ba2f3c68085e
Apolbaq <emilianopolanco5@gmail.com>
2021-10-18 01:50:59 +0200

    A lot of work and a lot of bugs fo fix

--- main.js
+++ main.js
@@ -1,12 +1,83 @@
 //Now I select the html elements intro variables
 const canvas = document.getElementById('board');
 const ctx = canvas.getContext('2d');
-
+const tetraminousArr = [[Z,'red'],[I,'cyan'],[J,'orange'],[L,'blue'],[O,'yellow'],[S,'green'],[T,'purple']];
 //Now the canvas has a grid with the rules.js constants
 ctx.canvas.width = cols*blockSize;
 ctx.canvas.height = rows*blockSize;
-//Scale property to scale the draw WxH
+//Scale property to scale the draw WxH****This is important because now all our units are : blocksize
 ctx.scale(blockSize, blockSize);
+ctx.lineWidth = 0.05;
+
+//Now I create the  the board
+
+let boardArr = [];
+
+for(let i = 0; i < rows; i++){
+    boardArr[i] = [];
+    for(let j = 0; j < cols; j++){
+        boardArr[i][j] = empty;
+    }
+}
+//Function that draws the board:
+drawBoard();
+function drawBoard(){
+    for(let i = 0; i<rows; i++){
+        for(let j = 0; j<cols; j++){
+            drawSquare(j, i, boardArr[i][j]);
+        }
+    }
+
+}
+
+
+//This function draws a 1 square in the grid
+function drawSquare(x,y,color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, 1, 1);
+    ctx.strokeStyle = 'rgb(156, 156, 156)';
+    ctx.strokeRect(x, y, 1, 1);
+}
+console.table(boardArr);
+//Creation of tetramino objects
+
+let tetraminoes = generateTetra();
+tetraminoes.drawTetrominos()
+
+
+//I add an even listener for the arrows press
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    switch (event.key){
+        case 'ArrowLeft':
+            tetraminoes.moveLeft();
+            break;
+        case 'ArrowRight':
+            tetraminoes.moveRight();
+            break;
+        case 'ArrowUp':
+            console.log('you can\'t move up');
+            break;
+        case 'ArrowDown':
+            tetraminoes.moveDown();
+            break;
+        case 'Enter':
+            tetraminoes.rotateRight();
+            console.log('enter');
+        default:
+    }
+});
+//Random tetramino generator function
+function generateTetra(){
+    let randomN = Math.floor(Math.random()*tetraminousArr.length);
+    return new Tetraminoes(tetraminousArr[randomN][0],tetraminousArr[randomN][1]);
+}
+//Time to drp the pieces
+function drop(){
+
+}
+
+
 
 
 
--- piece.js
+++ piece.js
@@ -1,16 +1,113 @@
-class Piece{
-    constructor(ctx){
-        this.ctx = ctx;
+//With this class Tetrominoes I will generate the instances and their properties
+class Tetraminoes{
+    constructor(type, color) {
+        // The type of tetromino
+        this.type = type;
+        // The position or rotation
+        this.position = 0;
+        //Color property
+        this.color = color;
+        //The type and position in realtime while playing
+        this.activePosition = this.type[this.position];
 
-        this.color = 'blue';
-        this.shape = [
-            [2,0,0],
-            [2,2,2],
-            [0,0,0]
-        ];
-
-        //Starting position
+        //These are the coordinates where all tetrominoes start
         this.x = 3;
-        this.y = 0;
+        this.y = -2;
+    }
+
+    //Methods for the tetraminoes
+
+    //Draw and undraw
+    //With this function draw tetrominow by type, and coordiantes
+    drawTetrominos(){
+        for(let i = 0; i<this.activePosition.length;i++){
+            for(let j = 0; j<this.activePosition.length; j++){
+                if(this.activePosition[i][j]===1){
+                    drawSquare(j+this.x,i+this.y,this.color);
+                    ctx.strokeStyle = this.color;
+                    ctx.strokeRect(j+this.x, i+this.y, 1, 1);
+                }//end if
+            }//end 2nd loop
+        }//end first loop
+    }//end function
+
+    //With this function undraw tetrominow by type, and coordiantes
+    undrawTetrominos(){
+        for(let i = 0; i<this.activePosition.length;i++){
+            for(let j = 0; j<this.activePosition.length; j++){
+                if(this.activePosition[i][j]===1){
+                    drawSquare(j+this.x,i+this.y,empty);
+                    ctx.strokeStyle = 'rgb(156, 156, 156)';
+                    ctx.strokeRect(j+this.x, i+this.y, 1, 1);
+                }//end if
+            }//end 2nd loop
+        }//end first loop
+    }//end function
+
+    //Movement of the pieces
+    moveLeft(){
+        if(!this.collision(-1,0)){
+            this.undrawTetrominos();
+            this.x--;
+            this.drawTetrominos();
+        }
+
+    }
+    moveRight(){
+        if(!this.collision(1,0)){
+            this.undrawTetrominos();
+            this.x++;
+            this.drawTetrominos();
+        }
+
+    }
+    moveDown(){
+        if(!this.collision(0,1)){
+            this.undrawTetrominos();
+            this.y++;
+            this.drawTetrominos();
+        }else{
+            this.lock();
+            tetraminoes = generateTetra();
+        }
+
+    }
+    //Collision detection function
+    collision(adX, adY){
+        for(let i = 0; i<this.activePosition.length;i++){
+            for(let j = 0; j<this.activePosition.length; j++){
+                if(this.activePosition[i][j]===1){
+                    if(this.x +j+adX<0 || this.x+j+adX>=cols || this.y+i+adY>=rows){
+                        return true;
+                    }
+                    if(this.y+i+adY<0){continue;}
+                    if(boardArr[this.y+i+adY][this.x+j]!==empty){
+                        return true;
+                     }
+                }
+            }//end 2nd loop
+        }//end first loop
+        return false;
+    }
+    //Rotation of the tetramino
+    rotateRight(){
+        this.undrawTetrominos();
+        this.position = (this.position+1)%this.type.length;
+        this.activePosition = this.type[this.position];
+        this.drawTetrominos();
+    }
+
+    //Lock the piece
+    lock(){
+        for(let i = 0; i<this.activePosition.length;i++){
+            for(let j = 0; j<this.activePosition.length; j++){
+                if(this.activePosition[i][j]===1){
+                    boardArr[this.y+i][this.x+j] = this.color;
+                    ctx.fillStyle = boardArr[this.y+i][this.x+j];
+                    ctx.fillRect(this.x+j, this.y+i, 1, 1);
+                }//end if
+            }//end 2nd loop
+        }//end first loop
+        console.table(boardArr);
     }
 }
